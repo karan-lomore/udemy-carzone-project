05475c91e70176ae01be273a69c73b371c13617a
karan-lomore <karan.lomore@metflux.in>
2022-07-02 00:51:38 +0530

    created pages and navigation links

--- carzone/settings.py
+++ carzone/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'carzone/static'),
+    ]
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
