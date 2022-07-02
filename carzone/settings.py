29b84669aae28e4bbb9a9d7bedf380560d578aa4
karan-lomore <karan.lomore@metflux.in>
2022-07-02 20:58:35 +0530

    completed Team model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,17 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'carzone_db',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'Bhagchand@11',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
@@ -112,7 +121,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -127,3 +136,9 @@ STATICFILES_DIRS = [
     ]
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+
+# media files
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,19 @@
+from distutils.command.build_scripts import first_line_re
+from distutils.command.upload import upload
+from ssl import create_default_context
+from venv import create
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    
+    first_name = models.CharField(max_length= 255, blank= False, null= False)
+    last_name = models.CharField(max_length= 255, blank= False, null= False)
+    designation = models.CharField(max_length= 255, blank= False, null= False)
+    photo = models.ImageField(upload_to= 'photos/%Y/%m/%d/')
+    facebook_link = models.URLField(max_length= 100)
+    twitter_link = models.URLField(max_length= 100)
+    google_plus_link = models.URLField(max_length= 100)
+    created_date = models.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return str(self.first_name)
